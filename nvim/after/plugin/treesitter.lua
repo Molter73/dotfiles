9f1a50dbf7a4f8dd4308ea9368d035011674264b
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-01-26 19:53:37 +0100

    Initial commit
    Moving dotfiles from the miscellaneous scripts repository

---
+++ nvim/after/plugin/treesitter.lua
@@ -0,0 +1,33 @@
+require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+        'bash',
+        'c',
+        'cpp',
+        'cmake',
+        'dockerfile',
+        'go',
+        'haskell',
+        'help',
+        'html',
+        'json',
+        'lua',
+        'make',
+        'python',
+        'regex',
+        'rust',
+        'toml',
+        'yaml',
+    },
+
+    highlight = {
+        enable = true,
+    },
+
+    indent = {
+        enable = true,
+    },
+})
+
+require('treesitter-context').setup({
+    max_lines = 10,
+})
