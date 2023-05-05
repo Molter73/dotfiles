9c1a777cb338bf567bbd9c1df2591c79db5838fe
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-05-05 11:42:25 +0200

    Don't require telescope.builtin before installing

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -29,21 +29,6 @@ local opts = {
     },
 }
 
-local keys = function()
-    local builtins = require('telescope.builtin')
-
-    return {
-        { '<Leader>ff', builtins.find_files,                noremap = true, desc = '[F]ind [F]iles' },
-        { '<Leader>fg', builtins.live_grep,                 noremap = true, desc = 'Live grep' },
-        { '<Leader>fb', builtins.buffers,                   noremap = true, desc = '[F]ind [B]uffer' },
-        { '<Leader>fs', builtins.grep_string,               noremap = true, desc = '[F]ind [S]tring' },
-        { '<Leader>fh', builtins.help_tags,                 noremap = true, desc = '[F]ind [H]elp tags' },
-        { '<Leader>fd', builtins.diagnostics,               noremap = true, desc = '[F]ind [D]iagnostics' },
-        { '<Leader>fk', builtins.keymaps,                   noremap = true, desc = '[F]ind [K]eymaps' },
-        { '<Leader>fl', builtins.current_buffer_fuzzy_find, noremap = true, desc = '[F]ind [L]ocal' },
-    }
-end
-
 return {
     {
         'nvim-telescope/telescope.nvim',
@@ -52,11 +37,24 @@ return {
             'nvim-lua/plenary.nvim',
             {
                 'nvim-telescope/telescope-fzf-native.nvim',
-                build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+                build =
+                'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
             },
         },
         opts = opts,
-        keys = keys,
+        keys = function()
+            local builtins = require('telescope.builtin')
+            return {
+                { '<Leader>ff', builtins.find_files,                noremap = true, desc = '[F]ind [F]iles' },
+                { '<Leader>fg', builtins.live_grep,                 noremap = true, desc = 'Live grep' },
+                { '<Leader>fb', builtins.buffers,                   noremap = true, desc = '[F]ind [B]uffer' },
+                { '<Leader>fs', builtins.grep_string,               noremap = true, desc = '[F]ind [S]tring' },
+                { '<Leader>fh', builtins.help_tags,                 noremap = true, desc = '[F]ind [H]elp tags' },
+                { '<Leader>fd', builtins.diagnostics,               noremap = true, desc = '[F]ind [D]iagnostics' },
+                { '<Leader>fk', builtins.keymaps,                   noremap = true, desc = '[F]ind [K]eymaps' },
+                { '<Leader>fl', builtins.current_buffer_fuzzy_find, noremap = true, desc = '[F]ind [L]ocal' },
+            }
+        end,
         cmd = 'Telescope',
     },
 }
