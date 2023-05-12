c3975843cdbfb3eb4fdf9cbac24ea6b9216ff268
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-05-12 12:32:00 +0200

    Add Google java style formatting
    This is only used in a single project for now, so config is a bit wonky.

--- nvim/after/ftplugin/java.lua
+++ nvim/after/ftplugin/java.lua
@@ -20,10 +20,43 @@ local on_attach = function(data, bufnr)
         { noremap = true, silent = true, buffer = bufnr, desc = '[F]o[R]mat buffer' })
 end
 
+local settings = {
+    java = {
+        format = {}
+    }
+}
+
+-- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local jdtls_cache = vim.fn.stdpath('cache') .. '/jdtls'
+local workspace_dir = jdtls_cache .. '/workspace/' .. project_name
+local config_dir = '/usr/local/Cellar/jdtls/1.23.0/libexec/config_mac/'
+
+if string.find(vim.api.nvim_buf_get_name(0), 'mastermind') then
+    settings.java.format = {
+        enabled = false,
+    }
+
+    on_attach = require('plugins.lsp.utils').on_attach
+else
+    -- Setting options
+    vim.opt_local.expandtab = false
+end
+
 local config = {
-    cmd = { 'jdtls' },
-    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+    cmd = {
+        'jdtls',
+        '--jvm-arg=-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '--jvm-arg=-Dosgi.bundles.defaultStartLevel=4',
+        '--jvm-arg=-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '--jvm-arg=-Dlog.protocol=true',
+        '--jvm-arg=-Dlog.level=ALL',
+        '-configuration', config_dir,
+        '-data', workspace_dir,
+    },
+    root_dir = require('jdtls.setup').find_root({ 'pom.xml', '.git' }),
     on_attach = on_attach,
+    settings = settings,
 }
 
 local jdtlsau = vim.api.nvim_create_augroup("JDTLS", { clear = true })
@@ -34,6 +67,3 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 })
 
 jdtls.start_or_attach(config)
-
--- Setting options
-vim.opt_local.expandtab = false
