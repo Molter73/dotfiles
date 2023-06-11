10e72fe54b5daa04b25b666e8cce7d8a8c21db23
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-06-11 13:08:40 +0200

    Use 'legacy' tag for fidget.nvim

--- nvim/lua/plugins/miscellaneous.lua
+++ nvim/lua/plugins/miscellaneous.lua
@@ -7,6 +7,7 @@ return {
     {
         'j-hui/fidget.nvim',
         event = 'VeryLazy',
+        tag = 'legacy',
         config = function()
             require('fidget').setup({})
         end,
