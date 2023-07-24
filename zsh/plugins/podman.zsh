1ff41a3bdbf48dde7051396cc0531900f89a0268
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-07-24 12:04:34 +0200

    Improve command for image escalation and silence egrep deprecation message

--- zsh/plugins/podman.zsh
+++ zsh/plugins/podman.zsh
@@ -1,6 +1,7 @@
 super_image() {
+    image=$(docker images | sed 1d | grep -v '<none>' | fzf -q "$1" | awk '{print $1 ":" $2}')
     tempfile="$(mktemp)"
-    podman save -o "$tempfile" "$1"
-    sudo podman import "$tempfile" "$1"
+    podman save -o "$tempfile" "$image"
+    sudo podman import "$tempfile" "$image"
     rm -f "$tempfile"
 }
