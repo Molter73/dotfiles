d08cdf7543a30c7e1f8611eba941ef434fd2107b
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-07-28 13:08:10 +0200

    Random improvements to zsh configuration

--- zsh/plugins/podman.zsh
+++ zsh/plugins/podman.zsh
@@ -1,7 +1,4 @@
 super_image() {
     image=$(docker images | sed 1d | grep -v '<none>' | fzf -q "$1" | awk '{print $1 ":" $2}')
-    tempfile="$(mktemp)"
-    podman save -o "$tempfile" "$image"
-    sudo podman import "$tempfile" "$image"
-    rm -f "$tempfile"
+    podman save "$image" | sudo podman load
 }
