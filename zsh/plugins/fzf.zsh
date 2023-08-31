ec08223dea2451ef8847614db2b61fe1eff9ee83
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-08-31 17:38:55 +0200

    Start using tmux
    Modified the 'repos' command to create individual tmux sessions instead
    of simply changing directories. Inspired by theprimagen's
    tmux-sessionizer:
    https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -7,7 +7,29 @@ export FZF_DEFAULT_OPTS=" \
 # Change to a repo from anywhere
 repos() {
     DIR="$(fd . "${GOPATH}/src/" --type d --max-depth 3 --min-depth 3 | fzf -q "$1")"
-    cd "${DIR}"
+
+    if [[ -z $DIR ]]; then
+        exit 0
+    fi
+
+    selected_name=$(basename "$DIR" | tr . _)
+    tmux_running=$(pgrep tmux)
+
+    if [[ -z $tmux_running ]]; then
+        tmux new-session -s $selected_name -c $DIR
+        exit 0
+    fi
+
+    if ! tmux has-session -t=$selected_name 2> /dev/null; then
+        tmux new-session -ds $selected_name -c $DIR
+    fi
+
+    if [[ -z $TMUX ]]; then
+        # We are not in tmux, attach to the existing session
+        tmux attach -t $selected_name
+    else
+        tmux switch-client -t $selected_name
+    fi
 }
 
 # Stop docker containers
