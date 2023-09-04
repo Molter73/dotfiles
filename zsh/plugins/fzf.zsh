dcaa89bc4508ea9a5d14def5aefdb5f6732d17d1
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-09-04 10:06:42 +0200

    Miscellaneous improvements

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -9,7 +9,7 @@ repos() {
     DIR="$(fd . "${GOPATH}/src/" --type d --max-depth 3 --min-depth 3 | fzf -q "$1")"
 
     if [[ -z $DIR ]]; then
-        exit 0
+        return 0
     fi
 
     selected_name=$(basename "$DIR" | tr . _)
@@ -17,7 +17,7 @@ repos() {
 
     if [[ -z $tmux_running ]]; then
         tmux new-session -s $selected_name -c $DIR
-        exit 0
+        return 0
     fi
 
     if ! tmux has-session -t=$selected_name 2> /dev/null; then
