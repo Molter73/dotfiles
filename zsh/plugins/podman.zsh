f39ee2f773864c0a5f9527ec7bc47c00edc8555b
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-09-18 11:05:17 +0200

    Don't attempt file upgrades if none was selected

--- zsh/plugins/podman.zsh
+++ zsh/plugins/podman.zsh
@@ -1,4 +1,9 @@
 super_image() {
     image=$(docker images | sed 1d | grep -v '<none>' | fzf -q "$1" | awk '{print $1 ":" $2}')
+
+    if [[ -z "$image" ]]; then
+        return 0
+    fi
+
     podman save "$image" | sudo podman load
 }
