9900e5dd524c0010b3661f712db2dc3597c6bd93
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-10-05 18:07:23 +0200

    Add hardtime.nvim

--- nvim/lua/plugins/miscellaneous.lua
+++ nvim/lua/plugins/miscellaneous.lua
@@ -26,4 +26,11 @@ return {
     {
         'christoomey/vim-tmux-navigator',
     },
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {
+            restriction_mode = "hint",
+        }
+    },
 }
