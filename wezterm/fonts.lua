f5ac8df9079dfd8f0922b9a2a58fff167d7fbd65
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-11-10 13:17:12 +0100

    Back to firacode until monaspace ligatures improve

---
+++ wezterm/fonts.lua
@@ -0,0 +1,38 @@
+return {
+    firacode = {
+        family = 'FiraCode Nerd Font Mono',
+        weight = 'Bold',
+        size = 14,
+        harfbuzz = {
+            "cv02",
+            "cv05",
+            "cv09",
+            "cv14",
+            "cv16",
+            "cv25",
+            "cv26",
+            "cv27",
+            "cv31",
+            "cv32",
+            "zero",
+            "ss04",
+            "ss05",
+            "ss03",
+            "ss07",
+            "ss09",
+        },
+    },
+    monaspace = {
+        family = 'Monaspace Neon',
+        weight = 'DemiBold',
+        size = 14,
+        harfbuzz = {
+            'ss01',
+            'ss02',
+            'ss03',
+            'ss04',
+            'ss07',
+            'ss08'
+        },
+    },
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,27 +7,14 @@ config.audible_bell = 'Disabled'
 config.check_for_updates = false
 
 -- Font configuration
--- FiraCode
---[[
-config.font = wezterm.font({
-    family = 'FiraCode Nerd Font Mono',
-    weight = 'Bold',
-})
-config.font_size = 14
-config.harfbuzz_features = {
-    "cv02", "cv05", "cv09", "cv14", "cv16", "cv25", "cv26", "cv27", "cv31", "cv32", "zero", "ss04", "ss05", "ss03",
-    "ss07", "ss09", }
---]]
+local font = require('fonts').firacode
 
--- Monaspace
 config.font = wezterm.font({
-    family = 'Monaspace Neon',
-    weight = 'DemiBold',
+    family = font.family,
+    weight = font.weight,
 })
-config.font_size = 14
-config.harfbuzz_features = {
-    'ss01', 'ss02', 'ss03', 'ss04', 'ss07', 'ss08'
-}
+config.font_size = font.size
+config.harfbuzz_features = font.harfbuzz
 
 -- Cursor configuration
 config.default_cursor_style = 'BlinkingBlock'
