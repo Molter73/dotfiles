133cf5a393827bbab9e8229d08536fc0d2a4c73b
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-11-20 15:52:15 +0100

    Update collector-redeploy.sh script
    - Removed runtime configuration things.
    - Ensure image used by the ds is the same as the one being built.

--- scripts/collector-redeploy.sh
+++ scripts/collector-redeploy.sh
@@ -5,23 +5,33 @@ set -eoxu pipefail
 COLLECTOR_PATH="${GOPATH}/src/github.com/stackrox/collector"
 pushd "${COLLECTOR_PATH}"
 
-make image
-kubectl -n stackrox delete configmaps collector-config || true
-kind load docker-image quay.io/stackrox-io/collector:"$(make tag)"
-kubectl -n stackrox delete pod -l app=collector
-
-sleep 5
-cat << EOF | kubectl -n stackrox apply -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: collector-config
-  namespace: stackrox
-data:
-  runtime_config.yaml: |
-    networking:
-        externalIps:
-          enable: true
+function create_patch() {
+    cat << EOF | jq --arg image "$COLLECTOR_IMAGE" '.spec.template.spec.containers.[0].image |= $image'
+{
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [
+          {
+            "name": "collector",
+            "image": "\$image",
+            "imagePullPolicy": "IfNotPresent"
+          }
+        ]
+      }
+    }
+  }
+}
 EOF
+}
+
+COLLECTOR_TAG="$(make tag)"
+COLLECTOR_IMAGE="quay.io/stackrox-io/collector:${COLLECTOR_TAG}"
+PATCH="$(create_patch)"
+
+make image
+kind load docker-image "${COLLECTOR_IMAGE}"
+kubectl -n stackrox patch ds collector -p "${PATCH}"
+kubectl -n stackrox delete pod -l app=collector
 
 popd
