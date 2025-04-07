38046fda3729b47633221097a756625ca3ba1fb8
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-04-07 12:43:36 +0200

    Add hotreload to collector-redeploy.sh

--- scripts/collector-redeploy.sh
+++ scripts/collector-redeploy.sh
@@ -2,11 +2,14 @@
 
 set -eoxu pipefail
 
+HOTRELOAD=${HOTRELOAD:-false}
 COLLECTOR_PATH="${GOPATH}/src/github.com/stackrox/collector"
 pushd "${COLLECTOR_PATH}"
 
-function create_patch() {
-    cat << EOF | jq --arg image "$COLLECTOR_IMAGE" '.spec.template.spec.containers.[0].image |= $image'
+function patch_image() {
+    cat << EOF | jq \
+        --arg image "$COLLECTOR_IMAGE" \
+        '.spec.template.spec.containers.[0].image |= $image'
 {
   "spec": {
     "template": {
@@ -25,13 +28,52 @@ function create_patch() {
 EOF
 }
 
+function patch_hotreload() {
+    cat << EOF | jq \
+        --arg hostPath "$COLLECTOR_PATH" \
+        '.spec.template.spec.volumes.[0].hostPath.path |= $hostPath'
+{
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [
+          {
+            "name": "collector",
+            "command": ["/host/src/cmake-build/collector/collector"],
+            "volumeMounts": [
+              {
+                "mountPath": "/host/src",
+                "name": "collector-src",
+                "readOnly": true
+              }
+            ]
+          }
+        ],
+        "volumes": [
+          {
+            "hostPath": {
+              "path": "\$hostPath",
+              "type": ""
+            },
+            "name": "collector-src"
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
+}
+
 COLLECTOR_TAG="$(make tag)"
 COLLECTOR_IMAGE="quay.io/stackrox-io/collector:${COLLECTOR_TAG}"
-PATCH="$(create_patch)"
 
-make image
+make image-dev
 kind load docker-image "${COLLECTOR_IMAGE}"
-kubectl -n stackrox patch ds collector -p "${PATCH}"
+kubectl -n stackrox patch ds collector -p "$(patch_image)"
+if [[ "${HOTRELOAD}" == "true" ]]; then
+    kubectl -n stackrox patch ds collector -p "$(patch_hotreload)"
+fi
 kubectl -n stackrox delete pod -l app=collector
 
 popd
