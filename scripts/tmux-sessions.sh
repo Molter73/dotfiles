293168490d351979d0bdb4fea5e11dcf5023f1b9
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-05-14 16:56:15 +0200

    Minor improvements to some tmux workflows

--- scripts/tmux-sessions.sh
+++ scripts/tmux-sessions.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 pick_session() {
     tmux list-sessions \
         -F "#{session_name}" \
-        -f "#{?#{m/r:-popup$,#{session_name}},0,1}" | fzf -q "${1}"
+        -f "#{?#{m/r:-popup$,#{session_name}},0,1}" \
+        | fzf \
+            --tmux "75%" \
+            --reverse \
+            --border=double \
+            --border-label=" sessions " \
+            --border-label-pos=10 \
+            -q "${1}"
 }
 
 if [[ -z $TMUX ]]; then
@@ -14,4 +21,8 @@ if [[ -z $TMUX ]]; then
 fi
 
 SESSION="$(pick_session "${1:-}")"
+if [[ -z "${SESSION:-}" ]]; then
+    exit 0
+fi
+
 tmux switch-client -t "${SESSION}"
--- scripts/tmux_repos.sh
+++ scripts/tmux_repos.sh
@@ -3,7 +3,19 @@
 set -o pipefail
 
 pick_dir() {
-    fd . "${GOPATH}/src/" --type d --max-depth 3 --min-depth 3 | fzf -q "$1"
+    fd . "${GOPATH}/src/" --type d --max-depth 3 --min-depth 3 \
+        | fzf \
+            --tmux "75%" \
+            --reverse \
+            --border=double \
+            --border-label=" repos " \
+            --border-label-pos=10 \
+            --preview="[ -f {}README.md ] && bat \
+                --color=always \
+                --wrap=never \
+                --style=numbers \
+                {}README.md || echo 'No README'" \
+            -q "$1"
 }
 
 new_session() {
@@ -16,7 +28,6 @@ new_session() {
 }
 
 DIR="$(pick_dir "${1:-}")"
-
 if [[ -z $DIR ]]; then
     exit 0
 fi
