543001f640fc616ae266f2d368edcff254654460
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-05-20 14:05:04 +0200

    Finally configure codecompanion.nvim

--- nvim/lua/plugins/companion.lua
+++ nvim/lua/plugins/companion.lua
@@ -1,7 +1,36 @@
 return {
     "olimorris/codecompanion.nvim",
-    config = true,
-    enabled = false,
+    cond = function(_)
+        return os.getenv('CODECOMPANION_URL') ~= nil
+    end,
+    opts = {
+        adapters = {
+            granite = function()
+                return require('codecompanion.adapters').extend('openai_compatible', {
+                    env = {
+                        url = os.getenv('CODECOMPANION_URL'),
+                    },
+                })
+            end
+        },
+        strategies = {
+            inline = {
+                adapter = 'granite',
+            },
+            chat = {
+                adapter = 'granite',
+            },
+        },
+    },
+    cmd = {
+        'CodeCompanion',
+        'CodeCompanionActions',
+        'CodeCompanionChat',
+    },
+    keys = {
+        { '<Leader>ac', '<cmd>CodeCompanionActions<cr>' },
+    },
+    enabled = true,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
