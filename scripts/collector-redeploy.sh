d2436c1353c7f9cd0e1b5ebc3dad31859c44aa47
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-06-27 12:45:23 +0200

    Back to rootfull podman
    Turns out running the collector integration tests is a PITA if we don't
    have a root container engine, so back to that we go.

--- scripts/collector-redeploy.sh
+++ scripts/collector-redeploy.sh
@@ -69,8 +69,7 @@ COLLECTOR_TAG="$(make tag)"
 COLLECTOR_IMAGE="quay.io/stackrox-io/collector:${COLLECTOR_TAG}"
 
 make image-dev
-podman save "${COLLECTOR_IMAGE}" > /tmp/collector.tar
-CONTAINER_CONNECTION=root kind load image-archive /tmp/collector.tar
+kind load docker-image "${COLLECTOR_IMAGE}"
 kubectl -n stackrox patch ds collector -p "$(patch_image)"
 if [[ "${HOTRELOAD}" == "true" ]]; then
     kubectl -n stackrox patch ds collector -p "$(patch_hotreload)"
